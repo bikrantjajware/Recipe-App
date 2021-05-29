599503d8b5a40606520386ca311cc15b0650b64b
bikrantjajware <bikrantjajware@gmail.com>
2021-05-30 01:17:08 +0530

    added create new tag end point

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin,CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from .serializers import TagSerializer
@@ -7,7 +7,7 @@ from core.models import Tag
 # Create your views here.
 
 
-class TagViewSet(viewsets.GenericViewSet,ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,ListModelMixin,CreateModelMixin):
     """ Viewset to manage tags"""
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
@@ -17,3 +17,7 @@ class TagViewSet(viewsets.GenericViewSet,ListModelMixin):
     def get_queryset(self):
         """ return objects for current user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """ customize create new tag function gets validated serializer as input parameter"""
+        serializer.save(user=self.request.user)
